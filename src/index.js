9d5bafd20c243e940c494a36abe6d93a024598b4
Rockchalk8985 <steven.foster10@gmail.com>
2020-08-14 14:06:39 -0500

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,32 +1,37 @@
-
 let todos = [
-    {
-        completed: false,
-        description: "Take Jim to the hair salon"
-    },
-    {
-        completed: true,
-        description: "Drop off wedding invitation at mailbox"
-    },
-    {
-        completed: false,
-        description: "Pick up the cake"
-    },
-    {
-        completed: false,
-        description: "Call the caterers"
-    }
+  {
+    completed: false,
+    description: "Take Jim to the hair salon",
+  },
+  {
+    completed: true,
+    description: "Drop off wedding invitation at mailbox",
+  },
+  {
+    completed: false,
+    description: "Pick up the cake",
+  },
+  {
+    completed: false,
+    description: "Call the caterers",
+  },
 ];
 
 function renderTodoApp() {
-    let app = document.querySelector("#app")
-    let h1 = document.createElement("h1")
-    // your code here
+  let app = document.querySelector("#app");
+  let h1 = document.createElement("h1");
+  let ul = document.createElement("ul");
 
-    h1.innerText = "Todo List"
+  h1.innerText = "Todo List";
 
-    app.appendChild(h1)
-    // and maybe some here
+  app.appendChild(h1);
+  app.appendChild(ul);
+  //loop through todos
+  for (let i = 0; i < todos.length; i++) {
+    let li = `<li><input type="checkbox">${todos[i].description}</input></li>`;
+    ul.innerHTML += li;
+  }
+
 }
 
-renderTodoApp()
+renderTodoApp();
